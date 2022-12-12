576e93dd08b8902b376f3f9365b490a55d24617f
pojermarko <106599676+pojermarko@users.noreply.github.com>
2022-12-12 13:21:10 +0100

    Counter logic work, incrse decrese and reset

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/css';
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
